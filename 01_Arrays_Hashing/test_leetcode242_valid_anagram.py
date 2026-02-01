bbc2a9cc42f358dbd76da55edf18c727b6d0d97c
sree <sreeraj_ec@hotmail.com>
2026-02-02 05:56:56 +0800

    Add initial test cases for Valid Anagram solution

--- 01_Arrays_Hashing/test_leetcode242_valid_anagram.py
+++ 01_Arrays_Hashing/test_leetcode242_valid_anagram.py
@@ -6,3 +6,22 @@ class TestLeetcode242(unittest.TestCase):
 
     def setUp(self):
         self.sol = Solution()
+
+    def testStringIsEmpty(self):
+        pass
+
+    def testTestisSingleChar(self):
+        pass
+
+    def testValidAnagram(self):
+        pass
+
+    def testNOTValidAnagram(self):
+        pass
+
+    def testDifferentStringLengths(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
